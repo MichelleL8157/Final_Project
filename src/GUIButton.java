7d8531ab026846bb8162a46eeafc4f45bb245d40
Michelle Liu <michellel246@nycstudents.net>
2022-06-14 00:28:17 -0400

    Renamed file

---
+++ src/GUIButton.java
@@ -0,0 +1,107 @@
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import javax.swing.JButton;
+
+public class GUIButton extends JButton {
+    private boolean over;
+    private Color color;
+    private Color colorOver;
+    private Color colorClick;
+    private Color borderColor;
+    private int radius = 0;
+
+    public GUIButton() {
+        setColor(Color.WHITE);
+        colorOver = new Color(242, 215, 213);
+        colorClick = new Color(230, 176, 170);
+        borderColor = new Color(217, 136, 128);
+        setContentAreaFilled(false);
+        addMouseListener(new MouseAdapter() {
+            @Override
+            public void mouseEntered(MouseEvent e) {
+                setBackground(colorOver);
+                over = true;
+            }
+
+            @Override
+            public void mouseExited(MouseEvent e) {
+                setBackground(color);
+                over = false;
+            }
+
+            @Override
+            public void mousePressed(MouseEvent e) {
+                setBackground(colorClick);
+            }
+
+            @Override
+            public void mouseReleased(MouseEvent e) {
+                if (over) {
+                    setBackground(colorOver);
+                } else {
+                    setBackground(color);
+                }
+            }
+        });
+    }
+
+    protected void paintComponent(Graphics graphics) {
+        Graphics2D g2 = (Graphics2D) graphics;
+        g2.setRenderingHint(RenderingHints.KEY_ANTIALIASING, RenderingHints.VALUE_ANTIALIAS_ON);
+        g2.setColor(borderColor);
+        g2.fillRoundRect(0, 0, getWidth(), getHeight(), radius, radius);
+        g2.setColor(getBackground());
+
+        g2.fillRoundRect(2, 2, getWidth() - 4, getHeight() - 4, radius, radius);
+        super.paintComponent(graphics);
+    }
+
+    public boolean isOver() {
+        return over;
+    }
+
+    public void setOver(boolean over) {
+        this.over = over;
+    }
+
+    public Color getColor() {
+        return color;
+    }
+
+    public void setColor(Color color) {
+        this.color = color;
+    }
+
+    public Color getColorOver() {
+        return color;
+    }
+
+    public void setColorOver(Color colorOver) {
+        this.colorOver = colorOver;
+    }
+
+    public Color getColorClick() {
+        return colorClick;
+    }
+
+    public void setColorClick(Color colorClick) {
+        this.colorClick = colorClick;
+    }
+
+    public Color getBorderColor() {
+        return borderColor;
+    }
+
+    public void setBorderColor(Color borderColor) {
+        this.borderColor = borderColor;
+    }
+
+    public int getRadius() {
+        return radius;
+    }
+
+    public void setRadius(int radius) {
+        this.radius = radius;
+    }
+}
